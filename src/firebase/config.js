42c744dff75701e5881cb965f7d826b5af3bdb10
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-08 18:31:47 -0500

    update config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,7 @@ import { includes } from "lodash";
 
 const hostName = window.location.hostname;
 
-const hostsProduction = ["facilfacturaweb.web.app"];
+const hostsProduction = ["factura.servitec.site"];
 
 const currentEnvironment = includes(hostsProduction, hostName)
   ? "production"
