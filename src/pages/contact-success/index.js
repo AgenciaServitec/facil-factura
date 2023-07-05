a85dd997c852af458bcd0a1380838a8f18bf574e
angelgt <angel.gamonal@agenciaservitec.com>
2023-07-05 16:13:58 -0500

    refactor script

--- src/pages/contact-success/index.js
+++ src/pages/contact-success/index.js
@@ -11,8 +11,20 @@ export const ContactSuccess = ({
 }) => {
   const navigate = useNavigate();
 
+  const insertScript = (scriptString) => {
+    const scriptElement = document.createElement("script");
+    scriptElement.innerHTML = scriptString;
+    return document.head.appendChild(scriptElement);
+  };
+
   useEffect(() => {
-    location.reload();
+    const scriptElement = insertScript(` 
+          gtag('event', 'conversion', {'send_to': 'AW-11215674135/GngjCM2NlrMYEJe2huQp'});
+     `);
+
+    return () => {
+      scriptElement.remove();
+    };
   }, []);
 
   useEffect(() => {
