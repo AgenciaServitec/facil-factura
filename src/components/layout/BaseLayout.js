24cfb05be159fe05289368d1b530ff42e209fa8e
noel <admin@agenciaservitec.com>
2024-04-19 00:51:51 -0500

    Add top promotion

--- src/components/layout/BaseLayout.js
+++ src/components/layout/BaseLayout.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { createPortal } from "react-dom";
 import styled, { css } from "styled-components";
 import { useDevice } from "../../hooks";
 import { Drawer } from "./Drawer";
@@ -6,6 +7,8 @@ import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { useNavigate } from "react-router";
 import { FormContact } from "../ui";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export const BaseLayout = ({ children }) => {
   const { isMobile } = useDevice();
@@ -18,6 +21,21 @@ export const BaseLayout = ({ children }) => {
 
   return (
     <Container>
+      {createPortal(
+        <PromotionContainer>
+          <div />
+          <div>
+            <span>
+              Quiero mi descuento del 30% por ser uno de los primeros 100
+              clientes nuevos. <a href="#">Click aqui!</a>
+            </span>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faXmark} size="lg" className="icon" />
+          </div>
+        </PromotionContainer>,
+        document.getElementById("promotion-wrapper")
+      )}
       <Drawer
         visibleDrawer={visibleDrawer}
         onSetVisibleDrawer={setVisibleDrawer}
@@ -48,11 +66,43 @@ export const BaseLayout = ({ children }) => {
 };
 
 const Container = styled.div`
-  ${({ theme }) => css`
-    width: 100%;
-    min-height: 100vh;
-    height: auto;
-    position: relative;
-    overflow: hidden;
-  `}
+  width: 100%;
+  min-height: 100vh;
+  height: auto;
+  position: relative;
+  overflow: hidden;
+`;
+
+const PromotionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0.6em;
+  text-align: center;
+  background: forestgreen;
+  font-size: 1em;
+  color: #fff;
+
+  div:nth-child(2) {
+    span {
+      cursor: pointer;
+
+      &:hover {
+        color: #e8e7e7;
+      }
+      a {
+        color: ${({ theme }) => theme.colors.primary};
+        text-decoration: none;
+      }
+    }
+  }
+
+  div:last-child {
+    padding: 0 1em;
+
+    .icon {
+      cursor: pointer;
+    }
+  }
 `;
